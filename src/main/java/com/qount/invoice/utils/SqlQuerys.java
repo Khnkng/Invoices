1a9220c960506775020949eb2ca1088200321cbf
siva09 <siva.maddineni@qount.io>
2017-07-20 11:49:01 +0530

    added journalID while retrieving invoice payments.

--- src/main/java/com/qount/invoice/utils/SqlQuerys.java
+++ src/main/java/com/qount/invoice/utils/SqlQuerys.java
@@ -88,7 +88,7 @@ public class SqlQuerys {
 
 	public final class Payments {
 		public static final String INSERT_QRY = "INSERT INTO invoice_payments ( `id`,`received_from`,`payment_amount`,`currency_code`,`reference_no`,`payment_date`,`memo`,`company_id`,`type`, `payment_notes`, `bank_account_id`) VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE `received_from` = ?, `payment_amount` = ?, `currency_code` = ?, `reference_no` = ?, `payment_date` = ?, `memo` = ?, `company_id` = ?,`type` = ?,`payment_notes` = ?, `bank_account_id` = ?";
-		public static final String RETRIEVE_BY_COMPANYID_QRY = "SELECT * FROM invoice_payments WHERE `company_id` = ?;";
+		public static final String RETRIEVE_BY_COMPANYID_QRY = "SELECT invoice_payments.*,journals.`id` AS journal_id,journals.`isActive` FROM invoice_payments LEFT JOIN journals ON `invoice_payments`.id = journals.`sourceID` WHERE `company_id`= ?;";
 		public static final String RETRIEVE_BY_PAYMENTID_QRY = "SELECT * FROM invoice_payments WHERE `id` = ?;";
 	}
 
