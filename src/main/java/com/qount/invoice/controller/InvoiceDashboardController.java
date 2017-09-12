c879e62dffc1e61761130b330bb62043513ba859
apurva <apurva.khune@qount.io>
2017-09-12 19:15:06 +0530

    added logic to get total receivables in invoices dashboard

---
+++ src/main/java/com/qount/invoice/controller/InvoiceDashboardController.java
@@ -0,0 +1,53 @@
+package com.qount.invoice.controller;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.log4j.Logger;
+
+import com.qount.invoice.controllerImpl.InvoiceDashboardControllerImpl;
+import com.qount.invoice.model.Invoice;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 
+ * @author Apurva, Qount.
+ * @version 1.0, 12 Sept 2016
+ *
+ */
+
+@Api(value = "invoice Dashboard Services")
+@Path("user/{userID}/companies/{companyID}/invoicedashboard")
+@Produces(MediaType.APPLICATION_JSON)
+public class InvoiceDashboardController {
+
+	private static final Logger LOGGER = Logger.getLogger(InvoiceDashboardController.class);
+
+	/**
+	 * 
+	 * @param userID
+	 * @param companyID
+	 * @return
+	 */
+	@Path("details")
+	@GET
+	@ApiOperation(value = "Used to  Get invoice Dashboard List", notes = "Used to Get invoice Dashboard List Based on CompanyID"
+			+ "<span class='bolder'>Sample Request:</span>" + "<div class='sample_response'>"
+			+ "json = {\"query param\":\"receivables\"}"
+			+ "</div>", responseContainer = "java.lang.String")
+	public List<Invoice> getInvoiceDashboardList(@PathParam("userID") String userID,
+			@PathParam("companyID") String companyID, @QueryParam("filter") String filter) {
+		LOGGER.debug("Entered into InvoiceDashboardController.getInvoiceDashboardList with  userID [ " + userID
+				+ " ] and companyID [ " + companyID + " ]");
+		return InvoiceDashboardControllerImpl.getInvoiceDashboardList(userID, companyID, filter);
+	}
+
+}
---
+++ src/main/java/com/qount/invoice/controllerImpl/InvoiceDashboardControllerImpl.java
@@ -0,0 +1,64 @@
+package com.qount.invoice.controllerImpl;
+
+/**
+ * 
+ * @author Apurva, Qount.
+ * @version 1.0, 12 Sept 2016
+ *
+ */
+
+import java.util.List;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
+import com.qount.invoice.database.mySQL.MySQLManager;
+import com.qount.invoice.model.Invoice;
+import com.qount.invoice.parser.InvoiceParser;
+import com.qount.invoice.utils.CommonUtils;
+import com.qount.invoice.utils.Constants;
+import com.qount.invoice.utils.ResponseUtil;
+import com.qount.invoice.utils.SqlQuerys;
+
+public class InvoiceDashboardControllerImpl {
+
+	private static final Logger LOGGER = Logger.getLogger(InvoiceDashboardControllerImpl.class);
+
+	public static List<Invoice> getInvoiceDashboardList(String userID, String companyID, String filter) {
+		LOGGER.debug("Entered into InvoiceDashboardControllerImpl.getInvoiceDashboardList with  userID [ " + userID
+				+ " ] and companyID [ " + companyID + " ]");
+		if (StringUtils.isAnyBlank(companyID, filter)) {
+			throw new WebApplicationException(ResponseUtil.constructResponse(Constants.FAILURE_STATUS_STR,
+					Constants.PRECONDITION_FAILED_STR + ":companyID and filter are mandatory",
+					Status.PRECONDITION_FAILED));
+		}
+		List<Invoice> result = null;
+		try {
+			String query = null;
+			if (filter == null || filter.equals("receivables")) {
+
+				query = SqlQuerys.Invoice.RETRIEVE_INVOICES_FOR_DASHBOARD_RECEIVABLES_QRY;
+
+			}
+			if (StringUtils.isEmpty(query)) {
+				throw new WebApplicationException(ResponseUtil.constructResponse(Constants.FAILURE_STATUS_STR,
+						Constants.PRECONDITION_FAILED_STR + ":check the query parameter", Status.PRECONDITION_FAILED));
+			}
+			List<Invoice> invoiceLst = MySQLManager.getInvoiceDAOInstance()
+					.retrieveInvoicesByCurrentStateAndCompany(companyID, query);
+			InvoiceParser.formatInvoices(invoiceLst);
+//			result = InvoiceParser.prepareInvoiceDashboardResponse(invoiceLst);
+			return invoiceLst;
+		} catch (Exception e) {
+			LOGGER.error(CommonUtils.getErrorStackTrace(e));
+			throw e;
+		} finally {
+			LOGGER.debug("Exited InvoiceDashboardControllerImpl.getInvoiceDashboardList with  userID [ " + userID
+					+ " ] and companyID [ " + companyID + " ] result [" + result + " ] ");
+		}
+	}
+
+}
