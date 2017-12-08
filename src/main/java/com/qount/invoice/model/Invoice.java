a068e60d2f07f6491fc30ef91c582f55ac37d230
MateenKhan <mateen.khan@qount.io>
2017-12-08 21:34:32 +0530

    updated invoice template

--- src/main/java/com/qount/invoice/model/Invoice.java
+++ src/main/java/com/qount/invoice/model/Invoice.java
@@ -89,7 +89,25 @@ public class Invoice {
 	private boolean late_fee_applied;
 	private String existing_late_fee_id;
 	private double existing_late_fee_amount;
+	private String customer_first_name;
+	private String customer_last_name;
 	
+	public String getCustomer_first_name() {
+		return customer_first_name;
+	}
+
+	public void setCustomer_first_name(String customer_first_name) {
+		this.customer_first_name = customer_first_name;
+	}
+
+	public String getCustomer_last_name() {
+		return customer_last_name;
+	}
+
+	public void setCustomer_last_name(String customer_last_name) {
+		this.customer_last_name = customer_last_name;
+	}
+
 	public String getExisting_late_fee_id() {
 		return existing_late_fee_id;
 	}
