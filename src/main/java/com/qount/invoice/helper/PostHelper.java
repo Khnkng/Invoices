fd2d45c374af0862ff39b7406acde4a7d9ed42a5
ravi devineni <ravi.devineni@qount.io>
2017-12-26 11:09:49 +0530

    Corrected LTM

--- src/main/java/com/qount/invoice/helper/PostHelper.java
+++ src/main/java/com/qount/invoice/helper/PostHelper.java
@@ -76,7 +76,7 @@ public class PostHelper {
 			path = path + "CollaborationServices/users/" + userId + "/companies/" + companyId + "/posts";
 			LOGGER.debug("path = " + path);
 			LOGGER.debug("payload = " + payload);
-			Response responseEntity = ClientBuilder.newClient().target(path).request().header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiI5MGUwZDU4Ny1kZjE2LTQ1YzgtOTExZC1jYjFlNDhmMDA4ZTMiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJxb3VudC5pbyIsImV4cCI6MTUyNjk5MTcwMiwianRpIjoiNDdlYzgxNTAtMDJlNy00MDBjLWE3YTctNDNkNmJmMTQ5OTg3IiwiaWF0IjoxNDk1NDU1NzAzLCJuYmYiOjE0OTU0NTU1ODMsInN1YiI6IntcImlkXCI6XCJyYXZpa2lyYW43MzhAZ21haWwuY29tXCIsXCJwaG9uZU51bWJlclwiOm51bGwsXCJwcm9maWxlUGljXCI6bnVsbCxcImNyZWF0ZWREYXRlXCI6bnVsbCxcIm1vZGlmaWVkRGF0ZVwiOm51bGwsXCJkZWZhdWx0Q29tcGFueVwiOm51bGwsXCJ0ZW1wUGFzc3dvcmRcIjpmYWxzZSxcInZlbmRvcklEXCI6bnVsbCxcImFjdGl2ZVwiOmZhbHNlLFwicGFzc3dvcmRcIjpudWxsLFwiYWRtaW5cIjpmYWxzZSxcImZpcnN0X25hbWVcIjpcIlJhdmlraXJhblwiLFwibGFzdF9uYW1lXCI6XCJEZXZpbmVuaVwifSJ9.xAWpRbt8ARab_YqgB70Y4KEk2K1NQ9HeYiffMeEs8xo").accept(MediaType.APPLICATION_JSON)
+			Response responseEntity = ClientBuilder.newClient().target(path).request().accept(MediaType.APPLICATION_JSON)
 					.post(Entity.entity(payload, MediaType.APPLICATION_JSON));
 			response = responseEntity.readEntity(String.class);
 			System.out.println("response " + response);
